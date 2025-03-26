f00af98d282a84af46e65da3268d9f66495077ec
filipkk78 <filipkk78@gmail.com>
2025-03-26 18:21:36 +0100

    Set up folder structure

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+// const indexRouter = require("./routes/indexRouter");
+
+// app.use("/", indexRouter);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Message board running on port ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
