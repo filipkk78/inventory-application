7519ad9765dcbc0d45b97afe8b76019d7a2e7dd8
filipkk78 <filipkk78@gmail.com>
2025-03-27 00:04:50 +0100

    Set up controller files and update routes

--- routes/genresRouter.js
+++ routes/genresRouter.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
 const { getGenres } = require("../controllers/getGenres.js");
-const { searchGenres } = require("../controllers/searchGenres.js");
 const genresRouter = Router();
 
 genresRouter.get("/", getGenres);
-genresRouter.get("/:id", searchGenres);
 
 module.exports = genresRouter;
