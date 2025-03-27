53297936ce2d0201a81eced1f5cec3997c560872
filipkk78 <filipkk78@gmail.com>
2025-03-27 19:01:22 +0100

    Add route for game details

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const { getGames } = require("../controllers/getGames.js");
+const { getGame } = require("../controllers/getGame.js");
 const gamesRouter = Router();
 
 gamesRouter.get("/", getGames);
+gamesRouter.get("/:gameid", getGame);
 
 module.exports = gamesRouter;
